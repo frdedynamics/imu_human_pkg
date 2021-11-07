7695f198eed41b60c3f67e03b6b41e80163b340d
Gizem <gizemates@iyte.edu.tr>
2021-11-07 20:57:50 +0100

    real robot/simulation selection added. Default link lengths added

--- src/qnode.py
+++ src/qnode.py
@@ -19,7 +19,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle("IMU based HRC")
         self.setWindowIcon(QIcon('../config/hvlLogo.png'))  # Only for icon on toolbar
-        
+
+        self.l_upper_trunk.setText("0.510")
+        self.l_upper_arm.setText("0.500")
+        self.l_forearm.setText("0.299")
+        self.l_hand.setText("0.203")
+
+        self.radioButton_simulation.setChecked(True)
+        self.comboBox_robot_name.addItems(['Panda', 'UR5e', 'KUKA iiwa'])
+        self.lineEdit_robot_ip.setDisabled(True)  
+
+        self.radioButton_real_robot.clicked.connect(self.real_robot_selected)
+        self.radioButton_simulation.clicked.connect(self.simulation_selected)
+    
+    def real_robot_selected(self):
+        self.textEdit.setText("Real robot selected")
+        self.comboBox_robot_name.setDisabled(True)
+        self.lineEdit_robot_ip.setEnabled(True)
+
+    def simulation_selected(self):
+        self.textEdit.setText("Simulation Selected")
+        self.lineEdit_robot_ip.setDisabled(True)
+        self.comboBox_robot_name.setEnabled(True)
+
+
 
 stylesheet = """
     MainWindow {
