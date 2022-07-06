1eaa24b58d783d21c571e14dc0b81ee8ef06dfde
Gizem <gizem.ates@hvl.no>
2022-07-06 14:04:42 +0200

    dummy publisher is not working, force mode blocking, test setup ready

--- imu_human_pkg/src/colift_test.py
+++ imu_human_pkg/src/colift_test.py
@@ -5,6 +5,9 @@ This is a node to test COLIFT state
 """
 
 import rospy
+from std_msgs.msg import Int64
+import subprocess, time
+from multiprocessing import Process, Queue
 
 from rtde_control import RTDEControlInterface as RTDEControl
 import rtde_receive
@@ -12,35 +15,57 @@ import rtde_receive
 rtde_c = RTDEControl("172.31.1.144", RTDEControl.FLAG_USE_EXT_UR_CAP)
 rtde_r = rtde_receive.RTDEReceiveInterface("172.31.1.144")
 
+# colift_proc = subprocess.Popen(["sh", "../sh/myo.sh"])
+
+test_count = Int64()
+
+
 def force_apply():
     f = 15
-    ''' Make force thingy here '''
-    # vector = self.rtde_r.getActualTCPPose() # A pose vector that defines the force frame relative to the base frame.
     vector = [0, 0, 0, 0, 0, 0]
-    selection_vector = [1, 1, 1, 0, 0, 0] # A 6d vector of 0s and 1s. 1 means that the robot will be compliant in the corresponding axis of the task frame
-    wrench = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0] # The forces/torques the robot will apply to its environment. The robot adjusts its position along/about compliant axis in order to achieve the specified force/torque. Values have no effect for non-compliant axes
-    type = 2 # An integer [1;3] specifying how the robot interprets the force frame. 1: The force frame is transformed in a way such that its y-axis is aligned with a vector pointing from the robot tcp towards the origin of the force frame. 2: The force frame is not transformed. 3: The force frame is transformed in a way such that its x-axis is the projection of the robot tcp velocity vector onto the x-y plane of the force frame.
-    limits = [f, f, f, 0.17, 0.17, 0.17]# (Float) 6d vector. For compliant axes, these values are the maximum allowed tcp speed along/about the axis. For non-compliant axes, these values are the maximum allowed deviation along/about an axis between the actual tcp position and the one set by the program.
+    type = 2 
+    selection_vector = [1, 0, 0, 0, 0, 0]
+    wrench = [-f, 0.0, 0.0, 0.0, 0.0, 0.0]
+    limits = [0.5, 0.3, 0.3, 0.17, 0.17, 0.17]
+
 
     _curr_force = rtde_r.getActualTCPForce()
-    print(_curr_force)
+    # print(_curr_force)
+    rtde_c.forceMode(vector, selection_vector, wrench, type, limits)
+
+    time.sleep(2)
+
+    rtde_c.forceModeStop()
+    time.sleep(2)
+    selection_vector = [1, 0, 0, 0, 0, 0]
+    wrench = [f, 0.0, 0.0, 0.0, 0.0, 0.0]
+    limits = [0.5, 0.3, 0.3, 0.17, 0.17, 0.17]
+    rtde_c.forceMode(vector, selection_vector, wrench, type, limits)
+    time.sleep(2)
 
 
-def update():
+
+def update(pub_test_cmd):
     force_apply()
-    # print("updated")
+    test_count.data = test_count.data + 1
+    pub_test_cmd.publish(test_count)
+    print(test_count)
 
 
 def main(): 
     rospy.init_node('colift_test')
     print("Colift test node started")
+    pub_test_cmd = rospy.Publisher('/test_in', Int64, queue_size=1)
     rate = rospy.Rate(10)
     try:
         while not rospy.is_shutdown():
-            update()
+            update(pub_test_cmd)
             rate.sleep()
     except KeyboardInterrupt:
+        rtde_c.forceModeStop()
+        rtde_c.disconnect()
         rospy.signal_shutdown("KeyboardInterrupt")
+        # rtde_c.servoStop()
         raise
         
 
